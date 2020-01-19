6cd4070314152fee5e0a5f9df04acf1fdbb11f23
gagrigoryan <grigoryang085@gmail.com>
2020-01-20 01:34:59 +0300

    Create model, controller, migrations for Tasks. Add relationship for user and task.

--- app/User.php
+++ app/User.php
@@ -19,6 +19,10 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+    public function tasks() {
+        return $this->hasMany('App\Task');
+    }
+
     /**
      * The attributes that should be hidden for arrays.
      *
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/tasks', 'UserController@myTasks')->name('tasks');
